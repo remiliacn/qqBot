39bba6f058f951822e9f82929e186944e24d4d40
Remilia Lu <bilibililvnima@gmail.com>
2020-08-06 19:05:00 +0800

    Enhancement: Enhance the experiment for new users.

--- qqBotCore.py
+++ qqBotCore.py
@@ -1,8 +1,32 @@
+from json import loads, dump
+from os import path
+from time import sleep
+
 import nonebot
 from nonebot.log import logger
-from os import path
+
 import config
-from json import loads, dump
+
+config_file = \
+"""
+from nonebot.default_config import *
+
+NICKNAME = {}
+consumer_key = '' # twitter API consumer key
+consumer_secret = '' # twitter API consumer secret
+access_token = '' # twitter API access token
+access_secret = '' # twitter API access secret
+user_name = '' # Pixiv login username
+password = '' # Pixiv login password
+
+itpk_key = '' # 茉莉机器人API key
+itpk_secret = '' # 茉莉机器人API secret
+
+SESSION_EXPIRE_TIMEOUT = timedelta(minutes=1)
+
+HOST = '127.0.0.1'
+PORT = 5700
+"""
 
 def register_true():
     if not path.exists('D:/dl/started.json'):
@@ -18,32 +42,15 @@ def register_true():
 
     if not path.exists('config.py'):
         logger.warning('No config file detected. Generating a template...')
-        config_file = \
-        """
-        from nonebot.default_config import *
-        
-        NICKNAME = {}
-        consumer_key = '' # twitter API consumer key
-        consumer_secret = '' # twitter API consumer secret
-        access_token = '' # twitter API access token
-        access_secret = '' # twitter API access secret
-        user_name = '' # Pixiv login username
-        password = '' # Pixiv login password
-        
-        itpk_key = '' # 茉莉机器人API key
-        itpk_secret = '' # 茉莉机器人API secret
-        
-        SESSION_EXPIRE_TIMEOUT = timedelta(minutes=1)
-        
-        HOST = '127.0.0.1'
-        PORT = 5700
-        """
 
-        with open('config.py', 'w+') as file:
+        with open('config.py', 'w+', encoding='utf-8') as file:
             file.write(config_file)
 
-if __name__ == '__main__':
-    register_true()
+        sleep(3)
+        logger.warning('Generation completed... Exiting the program. Please edit it!')
+        exit(1)
+
+def main():
     nonebot.init(config)
     nonebot.log.logger.setLevel('INFO')
     nonebot.load_plugins(
@@ -53,6 +60,10 @@ if __name__ == '__main__':
 
     logger.warning('Plugins successfully installed.')
     nonebot.run()
+
+if __name__ == '__main__':
+    register_true()
+    main()
 
 
 
