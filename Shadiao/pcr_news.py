bf4a9afe7295c982513a66016682116e83524724
Remilia Lu <bilibililvnima@gmail.com>
2020-09-30 08:17:35 +0800

    Misc: Remove PCR check while launching.

--- Shadiao/pcr_news.py
+++ Shadiao/pcr_news.py
@@ -7,7 +7,6 @@ class GetPCRNews:
         self.headers = {
             'User-Agent' : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36"
         }
-        self.first_update_id = self._get_update_id()
         self.info_not_available = '无搜索结果，请检查物品名是否存在国服PCR'
 
     def _get_update_id(self) -> int:
@@ -20,55 +19,6 @@ class GetPCRNews:
         json_data = page.json()
         return json_data['data'][0]['id']
 
-    async def if_new_releases(self) -> bool:
-        ID = self._get_update_id()
-        if ID == self.first_update_id or self.first_update_id == -1:
-            return False
-
-        self.first_update_id = ID
-        return True
-
-    async def get_content(self) -> str:
-        try:
-            page = requests.get(f'https://api.biligame.com/news/{self.first_update_id}', headers=self.headers, timeout=10)
-        except Exception as e:
-            logger.warning(f'An error occurred while fetching PCR news: {e}')
-            return '获取详细内容失败！'
-
-        page.encoding = 'utf-8'
-        result = page.json()['data']['content']
-        import re
-        syntax = re.compile(r'<.*?>')
-        img = re.findall(r'<img src="//(.*?)"', result)
-        downloaded = False
-        path = ''
-
-        if img:
-            try:
-                page = requests.get('http://' + img[0], headers=self.headers, timeout=10)
-                fileName = re.findall(r'game/(.*?)$', img[0])
-
-                if fileName:
-                    path = f'{os.getcwd()}/data/bilibiliPic/' + fileName[0]
-
-                page.raise_for_status()
-                
-                if path != '':
-                    if not os.path.exists(path):
-                        with open(path, 'wb') as f:
-                            f.write(page.content)
-
-                    downloaded = True
-                    
-            except Exception as e:
-                logger.warning("Error occurred when downloading: " + str(e))
-
-        result = re.sub(syntax, '', result).replace('\r\n', '\n').replace('&nbsp;', ' ').replace('\t', '')
-        result = re.sub(r'\n+', '\n', result)
-
-        if downloaded:
-            result += f'[CQ:image,file=file:///{path}]'
-        return result
 
     async def pcr_check(self, query) -> str:
         url = f'https://wiki.biligame.com/pcr/{query}'
